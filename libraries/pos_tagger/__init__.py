f02f53b8cd8daff90d4a162d378e2bb0a504fbe4
xuetf <476122294@qq.com>
2020-02-07 11:28:23 +0800

    text-calss

---
+++ libraries/pos_tagger/__init__.py
@@ -0,0 +1,3 @@
+from libraries.pos_tagger.dataset_readers import *
+from libraries.pos_tagger.models import *
+from libraries.pos_tagger.predictors import *
---
+++ libraries/text_classifier/__init__.py
@@ -0,0 +1,3 @@
+from libraries.text_classifier.dataset_readers import *
+from libraries.text_classifier.models import *
+from libraries.text_classifier.predictors import *
